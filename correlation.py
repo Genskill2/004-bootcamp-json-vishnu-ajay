7f4fdd06ff689e76cd3c401d163f27e923421099
Vishnu Ajay <vishnuajay27@gmail.com>
2021-07-01 05:04:40 +0530

    First attempt

--- correlation.py
+++ correlation.py
@@ -1,1 +1,53 @@
 # Add the functions in this file
+def load_journal(jsonfile):
+    import json
+    parsed = json.loads(jsonfile)
+    return parsed
+
+
+def compute_phi(filename, event):
+    import math
+    n11, n00, n10, n01 = 0, 0, 0, 0
+    n1p, n0p, np1, np0 = 0, 0, 0, 0
+    journal = load_journal(filename)
+    for i in range(90):
+        if journal[i]['squirrel'] is true:
+            np1 += 1
+        else:
+            np0 += 1
+    for i in range(90):
+        if event in journal[i]['events']:
+            n1p += 1
+            if journal[i]['squirrel'] is true:
+                n11 += 1
+            else:
+                n10 += 1
+        else:
+            n0p += 1
+            if journal[i]['squirrel'] is true:
+                n01 += 1
+            else:
+                n00 += 1
+    phi = (n11 * n00 - n10 * n01) / math.sqrt(n1p * n0p * np1 * np0)
+    return phi
+
+
+def compute_correlations(filename):
+    correlation_dict = {}
+    events = []
+    journal = load_journal(filename)
+    for i in range(90):
+        for event in journal[i]['events']:
+            events.append(event) if event not in events
+    for event in events:
+        correlation_dict[event] = compute_phi(filename, event)
+    return correlation_dict
+
+
+def diagnose(filename):
+    c_dict = compute_correlations(filename)
+    keymax = max(c_dict, key=c_dict.get)
+    keymin = min(c_dict, key=c_dict.get)
+    return keymax, keymin
+
+
